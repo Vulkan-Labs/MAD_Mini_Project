c8b8e89eaf394129c4def4a98139b39cae232024
Charuka Kaluarachchi <70794286+cdkaluarachchi@users.noreply.github.com>
2021-09-23 15:56:56 +0530

    added manageProducts page and button

--- app/src/main/java/com/mad/miniproject_teamvulkan/pharmacyMain.java
+++ app/src/main/java/com/mad/miniproject_teamvulkan/pharmacyMain.java
@@ -2,7 +2,11 @@ package com.mad.miniproject_teamvulkan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class pharmacyMain extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class pharmacyMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pharmacy_main);
+
+        Button button1 = findViewById(R.id.manageProductsBTN);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent manProd = new Intent(getApplicationContext(), manageProducts.class);
+                startActivity(manProd);
+            }
+        });
+
+
+
+
     }
 }
