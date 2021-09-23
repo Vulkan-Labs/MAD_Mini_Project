cc6def68da80ea721130394bf9368146ce03c73b
Praveen Peiris <praveen.peiris.pp@gmail.com>
2021-09-23 19:11:14 +0530

    Initial Review page UI

--- app/src/main/java/com/mad/miniproject_teamvulkan/productView.java
+++ app/src/main/java/com/mad/miniproject_teamvulkan/productView.java
@@ -2,13 +2,27 @@ package com.mad.miniproject_teamvulkan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class productView extends AppCompatActivity {
+
+    private Button move;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_view);
+        move=findViewById(R.id.review_btn);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(productView.this,Review_view.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
