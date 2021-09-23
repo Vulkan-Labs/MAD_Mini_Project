ffc60cb9e46a432390ac99b2b696bdc045e394d9
Charuka Kaluarachchi <70794286+cdkaluarachchi@users.noreply.github.com>
2021-09-23 17:23:52 +0530

    create,add,edit activities

---
+++ app/src/main/java/com/mad/miniproject_teamvulkan/editProduct.java
@@ -0,0 +1,14 @@
+package com.mad.miniproject_teamvulkan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_product);
+    }
+}
--- app/src/main/java/com/mad/miniproject_teamvulkan/manageProducts.java
+++ app/src/main/java/com/mad/miniproject_teamvulkan/manageProducts.java
@@ -2,7 +2,10 @@ package com.mad.miniproject_teamvulkan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class manageProducts extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class manageProducts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_products);
+
+
+
+        Button button1 = findViewById(R.id.addNewButton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addProd = new Intent(getApplicationContext(), addProduct.class);
+                startActivity(addProd);
+            }
+        });
+
+
+
+
+
     }
 }
