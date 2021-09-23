872301be359f208f898a63b16c1c7b85f3169567
Praveen Peiris <praveen.peiris.pp@gmail.com>
2021-09-23 18:44:53 +0530

    Button navigation works

--- app/src/main/java/com/mad/miniproject_teamvulkan/MainActivity.java
+++ app/src/main/java/com/mad/miniproject_teamvulkan/MainActivity.java
@@ -2,13 +2,26 @@ package com.mad.miniproject_teamvulkan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nav=findViewById(R.id.welcome_btn);
+        nav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 Intent intent = new Intent(MainActivity.this,productView.class);
+                 startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/mad/miniproject_teamvulkan/productView.java
+++ app/src/main/java/com/mad/miniproject_teamvulkan/productView.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ProductView extends AppCompatActivity {
+public class productView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
