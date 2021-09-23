d223d6908b3145f8e066bc63fdab45a6a3f1cf6e
Tivain <70811407+Tivain-W@users.noreply.github.com>
2021-09-24 03:18:28 +0530

    LoginActivity & SignUpActivity implemented & working

--- app/src/main/java/com/mad/miniproject_teamvulkan/LoginActivity.java
+++ app/src/main/java/com/mad/miniproject_teamvulkan/LoginActivity.java
@@ -1,14 +1,80 @@
 package com.mad.miniproject_teamvulkan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    /*private TextView forgotPassword;*/
+    private Button login_btn;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.input_email);
+        password = findViewById(R.id.input_password);
+
+        login_btn = findViewById(R.id.btn_login2);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+    }
+
+    private void loginUser(){
+
+        String login_email = email.getText().toString();
+        String login_password = password.getText().toString();
+
+        if(!login_email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(login_email).matches()){
+            if(!login_password.isEmpty()){
+                mAuth.signInWithEmailAndPassword(login_email,login_password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(LoginActivity.this, "Logged In Successfully!",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this, AddCategoryActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this,"Login Attempt Unsuccessful! Try Again!",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }else{
+                password.setError("Password field is empty!");
+            }
+        }else if(login_email.isEmpty()){
+            email.setError("Email field is empty!");
+        }else{
+            email.setError("Please enter a valid email address!");
+        }
     }
 }
--- app/src/main/java/com/mad/miniproject_teamvulkan/SignUpActivity.java
+++ app/src/main/java/com/mad/miniproject_teamvulkan/SignUpActivity.java
@@ -1,14 +1,78 @@
 package com.mad.miniproject_teamvulkan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
+    /*full_name, phone_no,*/
+    private EditText  email, password;
+    private Button sign_up_btn;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        email = findViewById(R.id.input_sign_up_email);
+        password = findViewById(R.id.input_sign_up_password);
+        sign_up_btn = findViewById(R.id.btn_sign_up2);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        sign_up_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
+
     }
+
+    private void createUser(){
+
+        String sign_up_email = email.getText().toString();
+        String sign_up_password = password.getText().toString();
+
+        if(!sign_up_email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(sign_up_email).matches()){
+            if(!sign_up_password.isEmpty()){
+                mAuth.createUserWithEmailAndPassword(sign_up_email,sign_up_password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(SignUpActivity.this, "User Registration Successful!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignUpActivity.this,"User Registration Unsuccessful! Try Again!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }else{
+                password.setError("Password field is empty.");
+            }
+        }else if(sign_up_email.isEmpty()){
+            email.setError("Email field is empty.");
+        }else {
+            email.setError("Please enter a valid email.");
+        }
+    }
+
 }
