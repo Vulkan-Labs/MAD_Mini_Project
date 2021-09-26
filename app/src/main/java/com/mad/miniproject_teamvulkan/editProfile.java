ce56cce9526978fc59a685a28c93efd38579a429
Damian-Cntr1 <damiansdemel@gmail.com>
2021-09-26 11:28:17 +0530

    addCard test1

---
+++ app/src/main/java/com/mad/miniproject_teamvulkan/editProfile.java
@@ -0,0 +1,33 @@
+package com.mad.miniproject_teamvulkan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class editProfile {
+
+    EditText ;
+    Button ;
+    DatabaseReference dbRef;
+    payment pay;
+
+}
---
+++ app/src/main/java/com/mad/miniproject_teamvulkan/payment.java
@@ -0,0 +1,58 @@
+package com.mad.miniproject_teamvulkan;
+
+import com.google.firebase.database.Exclude;
+
+public class payment {
+
+    @Exclude
+    private String cardholderName;
+    private int cardNumber;
+    private String expDate;
+    private int cvv;
+    private String billingAddress;
+
+    public payment() {
+
+
+    }
+
+    public String getCardholderName() {
+        return cardholderName;
+    }
+
+    public void setCardholderName(String cardholderName) {
+        this.cardholderName = cardholderName;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(String expDate) {
+        this.expDate = expDate;
+    }
+
+    public int getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+}
