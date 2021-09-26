258c01d8a2590bf508174765513f9899f9aab713
Tivain <70811407+Tivain-W@users.noreply.github.com>
2021-09-26 11:59:57 +0530

    Added Functionality to LoginActivity to redirect users to relevant activity based on their account type.

--- app/src/main/java/com/mad/miniproject_teamvulkan/LoginActivity.java
+++ app/src/main/java/com/mad/miniproject_teamvulkan/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,11 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -60,6 +66,8 @@ public class LoginActivity extends AppCompatActivity {
 
         String login_email = email.getText().toString();
         String login_password = password.getText().toString();
+        //final String[] accType = new String[1];
+        //String accTypeString;
 
         if(!login_email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(login_email).matches()){
             if(!login_password.isEmpty()){
@@ -67,9 +75,39 @@ public class LoginActivity extends AppCompatActivity {
                         .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                             @Override
                             public void onSuccess(AuthResult authResult) {
-                                Toast.makeText(LoginActivity.this, "Logged In Successfully!",Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(LoginActivity.this, ManageCategoriesActivity.class));
-                                finish();
+
+                                //Getting User Account Type from DB
+
+                                String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                                DatabaseReference readRef = FirebaseDatabase.getInstance().getReference().child("UserDetails").child(userID);
+                                readRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                                        if (snapshot.hasChild("accountType")) {
+                                            String accTypeString = snapshot.child("accountType").getValue(String.class);
+                                            //For Testing Purposes Only
+                                            //Toast.makeText(LoginActivity.this,userID,Toast.LENGTH_SHORT).show();
+
+                                            if(accTypeString.equals("Pharmacy")){
+                                                Toast.makeText(LoginActivity.this, "Logged In Successfully!",Toast.LENGTH_SHORT).show();
+                                                startActivity(new Intent(LoginActivity.this, ManageCategoriesActivity.class));
+                                                finish();
+                                            }else if(accTypeString.equals("Customer")){
+                                                Toast.makeText(LoginActivity.this, "Logged In Successfully!",Toast.LENGTH_SHORT).show();
+                                                startActivity(new Intent(LoginActivity.this, AddCategoryActivity.class));
+                                                finish();
+                                            }
+                                        }
+
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+
                             }
                         }).addOnFailureListener(new OnFailureListener() {
                     @Override
